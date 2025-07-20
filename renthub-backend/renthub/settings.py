2c509ff98b46c32cfc6877d48c14ea5ecdc6d6f4
David Jose  Manzano Arias <davidjosemanzano@gmail.com>
2025-07-19 19:21:52 -0500

    Cambio de imagen base para backend por vulnerabilidad

--- renthub-backend/renthub/settings.py
+++ renthub-backend/renthub/settings.py
@@ -6,7 +6,6 @@ import os
 # Load environment variables from .env file
 # load_dotenv()
 
-# Gestion de Imagenes
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = "/media/"
